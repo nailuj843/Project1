c1af5e994562e37314a40ccacde26f5a5bf3383b
Julian Heinze <nailuj843@yahoo.com>
2021-06-14 16:23:48 -0400

    julian edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Julian was here
         </a>
       </header>
     </div>
